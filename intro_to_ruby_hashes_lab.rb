61951c9ad4a573c7f8102ba10f10bf57b210ef6f
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-05 19:18:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,10 @@ def monopoly_with_third_tier
 	{:railroads =>
 		{
 			:pieces => 4,
-			:rent_in_dollars => 25,
-			:two_pieces_owned => 50
+			:rent_in_dollars => {
+				:one_pieces_owned => 25,
+				:two_pieces_owned => 50
+			}
 		}
 	}
 end
